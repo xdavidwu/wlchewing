244a3b499195e3053bf5a3e8dd59bb7479fe6644
Pinghao Wu <xdavidwuph@gmail.com>
2025-08-16 11:15:33 +0800

    xmem: make it inline-able

--- xmem.h
+++ xmem.h
@@ -2,11 +2,26 @@
 #define XMEM_H
 
 #include <stddef.h>
+#include <stdlib.h>
+
+#include "wlchewing.h"
 
 #define xcalloc(...) _xcalloc(__FILE__, __LINE__, ##__VA_ARGS__)
 #define xstrdup(...) _xstrdup(__FILE__, __LINE__, ##__VA_ARGS__)
 
-void *_xcalloc(const char *f, int l, size_t nmemb, size_t size);
-char *_xstrdup(const char *f, int l, const char *s);
+static inline void *assert_pointer(const char *f, int l, const char *op, void *p) {
+	if (p == NULL) {
+		_wlchewing_errloc("Fail to %s", f, l, op);
+		exit(EXIT_FAILURE);
+	}
+	return p;
+}
 
+[[maybe_unused]] static inline void *_xcalloc(const char *f, int l, size_t nmemb, size_t size) {
+	return assert_pointer(f, l, &__func__[2], calloc(nmemb, size));
+}
+
+[[maybe_unused]] static inline char *_xstrdup(const char *f, int l, const char *s) {
+	return assert_pointer(f, l, &__func__[2], strdup(s));
+}
 #endif
